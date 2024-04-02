5d4383af8589444feed56ce1210427523e7591bd
Comma Device <device@comma.ai>
2024-04-02 10:17:18 +0000

    manager.py

--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -23,14 +23,14 @@ from openpilot.system.version import get_build_metadata, terms_version, training
 def manager_init() -> None:
   save_bootlog()
 
-#  build_metadata = get_build_metadata():
+  build_metadata = get_build_metadata()
 
   params = Params()
   params.clear_all(ParamKeyType.CLEAR_ON_MANAGER_START)
   params.clear_all(ParamKeyType.CLEAR_ON_ONROAD_TRANSITION)
   params.clear_all(ParamKeyType.CLEAR_ON_OFFROAD_TRANSITION)
-#  if build_metadata.release_channel:
-#    params.clear_all(ParamKeyType.DEVELOPMENT_ONLY)
+  if build_metadata.release_channel:
+    params.clear_all(ParamKeyType.DEVELOPMENT_ONLY)
 
   default_params: list[tuple[str, str | bytes]] = [
     ("CompletedTrainingVersion", "0"),
@@ -61,15 +61,15 @@ def manager_init() -> None:
     print("WARNING: failed to make /dev/shm")
 
   # set version params
-  params.put("Version",  Params().get("Version", encoding='utf-8'))
+  params.put("Version", build_metadata.openpilot.version)
   params.put("TermsVersion", terms_version)
   params.put("TrainingVersion", training_version)
-  params.put("GitCommit",  Params().get("GitCommit", encoding='utf-8'))
-  params.put("GitCommitDate",  Params().get("GitCommitDate", encoding='utf-8'))
-  params.put("GitBranch",  Params().get("GitBranch", encoding='utf-8'))
-  params.put("GitRemote",  Params().get("GitRemote", encoding='utf-8'))
-#  params.put_bool("IsTestedBranch", build_metadata.tested_channel)
-#  params.put_bool("IsReleaseBranch", build_metadata.release_channel)
+  params.put("GitCommit", build_metadata.openpilot.git_commit)
+  params.put("GitCommitDate", build_metadata.openpilot.git_commit_date)
+  params.put("GitBranch", build_metadata.channel)
+  params.put("GitRemote", build_metadata.openpilot.git_origin)
+  params.put_bool("IsTestedBranch", build_metadata.tested_channel)
+  params.put_bool("IsReleaseBranch", build_metadata.release_channel)
 
   # set dongle id
   reg_res = register(show_spinner=True)
@@ -79,22 +79,22 @@ def manager_init() -> None:
     serial = params.get("HardwareSerial")
     raise Exception(f"Registration failed for device {serial}")
   os.environ['DONGLE_ID'] = dongle_id  # Needed for swaglog
-#  os.environ['GIT_ORIGIN'] = build_metadata.openpilot.git_normalized_origin # Needed for swaglog
-#  os.environ['GIT_BRANCH'] = build_metadata.channel # Needed for swaglog
-#  os.environ['GIT_COMMIT'] = build_metadata.openpilot.git_commit # Needed for swaglog
+  os.environ['GIT_ORIGIN'] = build_metadata.openpilot.git_normalized_origin # Needed for swaglog
+  os.environ['GIT_BRANCH'] = build_metadata.channel # Needed for swaglog
+  os.environ['GIT_COMMIT'] = build_metadata.openpilot.git_commit # Needed for swaglog
 
-#  if not build_metadata.openpilot.is_dirty:
-#    os.environ['CLEAN'] = '1'
+  if not build_metadata.openpilot.is_dirty:
+    os.environ['CLEAN'] = '1'
 
   # init logging
   sentry.init(sentry.SentryProject.SELFDRIVE)
-#  cloudlog.bind_global(dongle_id=dongle_id,
-#                       version=build_metadata.openpilot.version,
-#                       origin=build_metadata.openpilot.git_normalized_origin,
-#                       branch=build_metadata.channel,
-#                       commit=build_metadata.openpilot.git_commit,
-#                       dirty=build_metadata.openpilot.is_dirty,
-#                       device=HARDWARE.get_device_type())
+  cloudlog.bind_global(dongle_id=dongle_id,
+                       version=build_metadata.openpilot.version,
+                       origin=build_metadata.openpilot.git_normalized_origin,
+                       branch=build_metadata.channel,
+                       commit=build_metadata.openpilot.git_commit,
+                       dirty=build_metadata.openpilot.is_dirty,
+                       device=HARDWARE.get_device_type())
 
   # preimport all processes
   for p in managed_processes.values():
